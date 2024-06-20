91317a21bc0dbf02a87c082569fe58851f8d2a5f
tay <phuonggteyy@gmail.com>
2024-06-20 14:32:16 +0700

    validate data

--- src/main/java/com/tay/controller/UserController.java
+++ src/main/java/com/tay/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.tay.controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,34 +13,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tay.dto.request.UserRequestDTO;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 	
 	@PostMapping("/")
-	public String addUser(@RequestBody UserRequestDTO userDTO) {
+	public String addUser(@Valid @RequestBody UserRequestDTO userDTO) {
 		return "User added";
 	}
 	
 	@PutMapping("/{userId}")
-	public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO userDTO) {
+	public String updateUser(@PathVariable int userId, @Valid @RequestBody UserRequestDTO userDTO) {
 		return "User updated";
 	}
 	
     @PatchMapping("/{userId}")
-    public String updateStatus(@PathVariable int userId, @RequestParam(required = false) boolean status) {
+    public String updateStatus(@PathVariable int userId, @Valid @RequestParam boolean status) {
         return "User Status changed";
     }
     
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable int userId){
+    public String deleteUser(@Min(1) @PathVariable int userId){
         return "User deleted";
     }
-
-    @GetMapping("/{userId}")
-    public UserRequestDTO getUser(@PathVariable int userId) {
-        return new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789");
-    }
+//
+//    @GetMapping("/{userId}")
+//    public UserRequestDTO getUser(@PathVariable int userId) {
+//        return new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789", "09/11/2003", ["spring"]);
+//    }
     
 
 }
--- src/main/java/com/tay/dto/request/UserRequestDTO.java
+++ src/main/java/com/tay/dto/request/UserRequestDTO.java
@@ -1,7 +1,18 @@
 package com.tay.dto.request;
 
-import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.tay.util.PhoneNumber;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +20,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserRequestDTO implements Serializable {
+public class UserRequestDTO {
+
+	// error message should contain field name that is not valid
 	
+	@NotBlank(message = "firstName must be not blank")
 	private String firstName;
-	private String className;
+
+	@NotNull(message = "lastName must be not null")
+	private String lastName;
+
+	@Email(message = "email invalid format")
 	private String email;
+
+	//@Pattern(regexp = "^\\d{10}$", message = "phone invalid format")
+	@PhoneNumber
 	private String phone;
 
+	@NotNull(message = "dateOfBirth must be not null")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@JsonFormat(pattern = "MM/dd/yyyy")
+	private Date dateOfBirth;
+
+	@NotEmpty
+	List<String> permission;
 }
