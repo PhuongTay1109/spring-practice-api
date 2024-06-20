fffc8387ceb61a3e627b8691bf80fc41aa3831ec
tay <phuonggteyy@gmail.com>
2024-06-20 17:10:14 +0700

    response data

--- src/main/java/com/tay/controller/UserController.java
+++ src/main/java/com/tay/controller/UserController.java
@@ -14,13 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tay.dto.request.UserRequestDTO;
-import com.tay.dto.response.ResponseDTO;
-import com.tay.dto.response.ResponseFailure;
 import com.tay.dto.response.ResponseSuccess;
 import com.tay.model.UserStatus;
 import com.tay.model.UserType;
 
-import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 
@@ -28,58 +25,30 @@ import jakarta.validation.constraints.Min;
 @RequestMapping("/user")
 @Validated
 public class UserController {
-	
-	// hỗ trợ viết document bằng OPEN API
-	@Operation(method = "POST", summary = "Add new user", description = "Send a request via this API to create new user")
-    @PostMapping(value = "/")
-    public ResponseDTO<Integer> addUser(@Valid @RequestBody UserRequestDTO user) {
-        return new ResponseDTO<>(HttpStatus.CREATED.value(), "user added successfully", 1);
-    }
 
-//    @PostMapping("/")
-//    public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO user) {
-//        try {
-//            return new ResponseSuccess(HttpStatus.CREATED, "User added successfully,", 1);
-//        } catch (Exception e) {
-//            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
-//        }
-//    }
+    @PostMapping("/")
+    public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO user) {
+        return new ResponseSuccess(HttpStatus.CREATED, "User added successfully,", 1);
+    }
 
     @PutMapping("/{userId}")
     public ResponseSuccess updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
-        try {
-            return new ResponseSuccess(HttpStatus.ACCEPTED, "User updated successfully");
-        } catch (Exception e) {
-            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
+        return new ResponseSuccess(HttpStatus.ACCEPTED, "User updated successfully");
     }
 
     @PatchMapping("/{userId}")
     public ResponseSuccess updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
-        try {
-            return new ResponseSuccess(HttpStatus.ACCEPTED, "User's status changed successfully");
-        } catch (Exception e) {
-            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
+        return new ResponseSuccess(HttpStatus.ACCEPTED, "User's status changed successfully");
     }
 
     @DeleteMapping("/{userId}")
     public ResponseSuccess deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
-        try {
-            return new ResponseSuccess(HttpStatus.NO_CONTENT, "User deleted successfully");
-        } catch (Exception e) {
-            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
+        return new ResponseSuccess(HttpStatus.NO_CONTENT, "User deleted successfully");
     }
 
     @GetMapping("/{userId}")
     public ResponseSuccess getUser(@PathVariable @Min(1) int userId) {
-        
-        try {
-            return new ResponseSuccess(HttpStatus.OK, "user", new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789", UserStatus.ACTIVE, UserType.ADMIN, null, "PhuongTay", "phuongtay", null));
-        } catch (Exception e) {
-            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
+    	return new ResponseSuccess(HttpStatus.OK, "user", new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789", UserStatus.ACTIVE, UserType.ADMIN, null, "PhuongTay", "phuongtay", null));
     }
 
 }
--- src/main/java/com/tay/dto/response/ResponseFailure.java
+++
@@ -1,9 +0,0 @@
-package com.tay.dto.response;
-
-import org.springframework.http.HttpStatus;
-
-public class ResponseFailure extends ResponseSuccess{
-    public ResponseFailure(HttpStatus status, String message) {
-        super(status, message);
-    }
-}
